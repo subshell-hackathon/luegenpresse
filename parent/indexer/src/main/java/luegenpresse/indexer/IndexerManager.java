eb011dea0281395242255c90cff3ad62171acac8
Jan Rieke <rieke@subshell.com>
2016-02-26 17:52:34 +0100

    all regional NDR RSS feed

--- parent/indexer/src/main/java/luegenpresse/indexer/IndexerManager.java
+++ parent/indexer/src/main/java/luegenpresse/indexer/IndexerManager.java
@@ -14,26 +14,30 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Component;
 
 import luegenpresse.indexer.ts.NDRIndexer;
+import luegenpresse.indexer.ts.TagesschauIndexer;
 import luegenpresse.news.INewsRepository;
 
 @Component
 public class IndexerManager {
-	
+
 	private static int INDEXER_DELAY_MILLIS = 1 * 60 * 1000;
 
 	@Autowired
 	private INewsRepository repo;
-	
+
 	private ThreadPoolTaskScheduler scheduler;
-	
+
 	private Set<IIndexer> indexers;
-	
+
 	@PostConstruct
 	private void init() throws MalformedURLException {
 		indexers = new HashSet<IIndexer>(Arrays.asList(
-//					new TagesschauIndexer(),
-					new NDRIndexer(new URL("http://www.ndr.de/nachrichten/niedersachsen/index-rss.xml")))
-					);
+				new TagesschauIndexer(),
+				new NDRIndexer(new URL("http://www.ndr.de/nachrichten/niedersachsen/index-rss.xml")),
+				new NDRIndexer(new URL("http://www.ndr.de/nachrichten/schleswig-holstein/index-rss.xml")),
+				new NDRIndexer(new URL("http://www.ndr.de/nachrichten/mecklenburg-vorpommern/index-rss.xml")),
+				new NDRIndexer(new URL("http://www.ndr.de/nachrichten/hamburg/index-rss.xml")))
+				);
 		 
 		scheduler = new ThreadPoolTaskScheduler();
 		scheduler.setPoolSize(1);
@@ -42,10 +46,9 @@ public class IndexerManager {
 
 		indexers.forEach(indexer -> scheduler.scheduleWithFixedDelay(() -> indexer.runPeriodically(repo), INDEXER_DELAY_MILLIS));
 	}
-	
+
 	@PreDestroy
 	private void destroy() {
 		scheduler.shutdown();
 	}
-	
 }
