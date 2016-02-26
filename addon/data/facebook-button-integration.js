b7c8e7c69a5a25bc60b28341b47526db3175c9ba
Vasilis Ikonomou <vasilisikonomou@gmail.com>
2016-02-26 15:34:25 +0100

    Erster Stand eines Dummy-Add-ons, das in einem geöffneten Facebook-Tab Aktionen ausführen kann.

---
+++ addon/data/facebook-button-integration.js
@@ -0,0 +1,4 @@
+//	$(".jsb").css("border", "3px solid yellow");
+//	$("<a href=\"javascript:window.alert('Hallo Konrad!');\">OPEN WINDOW</a>").appendTo(".jsb");
+//	document.body.style.border = "5px solid red";
+window.alert("Sup?");
--- addon/index.js
+++ addon/index.js
@@ -1,9 +1,55 @@
 var self = require("sdk/self");
+var tabs = require("sdk/tabs");
+var panels = require("sdk/panel");
+var { ToggleButton } = require('sdk/ui/button/toggle');
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
+var isFacebookTabOpen = -1;
+var integratedForActiveTab = false;
+
+// Listen for tab content loads.
+tabs.on('ready', function(tab) {
+	isFacebookTabOpen = tab.url.indexOf("facebook.com");
+	integratedForActiveTab = false;
+});
+
+var panel = panels.Panel({
+	contentURL : self.data.url("wrong-active-tab-panel.html"),
+	onHide : uncheckButton
+});
+
+var button = ToggleButton({
+	id : "lyingPressButton",
+	label : "Was sagt die Lügenpresse dazu?",
+	icon : {
+		"16" : "./icon_16.png",
+		"32" : "./icon_32.png",
+		"64" : "./icon_64.png"
+	},
+	onChange : handleChange
+});
+
+function handleChange(state) {
+	if (state.checked) {
+		if (isFacebookTabOpen < 0) {
+			panel.show({
+				position : button
+			});
+		} else if (!integratedForActiveTab) {
+			console.log('Integrating buttons...');
+			require("sdk/tabs").activeTab.attach({
+				contentScriptFile : [ self.data.url("jquery-2.2.1.min.js"),
+				                      self.data.url("facebook-button-integration.js") ]
+			});
+			integratedForActiveTab = true;
+			uncheckButton();
+		} else {
+			uncheckButton();
+		} 
+	}
 }
 
-exports.dummy = dummy;
+function uncheckButton() {
+	button.state('window', {
+		checked : false
+	});
+}
