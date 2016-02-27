397d5f6feb5a4a255a69d080e9a5fdd6ac8ac32c
Oliver Behncke <behncke@subshell.com>
2016-02-27 14:49:21 +0100

    Fixed startup

--- addon/index.js
+++ addon/index.js
@@ -53,7 +53,7 @@ function handleChange(state) {
 				contentScriptOptions : {
 					borderUrl : self.data.url("images/border.png"),
 					logoUrl : self.data.url("images/icon_32.png"),
-					buttonLogoUrl : self.data.url("images/icon_16.png")
+					buttonLogoUrl : self.data.url("images/icon_16.png"),
 					noImageUrl : self.data.url("images/BuddyPreview.png")
 				}
 			});
